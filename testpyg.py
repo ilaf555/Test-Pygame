89833f8bac04122e5908a2a618e3db6d3e07c8fe
ilaf555 <37274152+ilaf555@users.noreply.github.com>
2023-01-04 04:00:36 +0300

    Create testpyg.py

---
+++ testpyg.py
@@ -0,0 +1,9 @@
+import pygame
+
+run = True
+width = 400
+height = 100
+pygame.init()
+screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont(None, 48)
+text = font.render("Welcome to pygame", True, (255, 255, 255))
